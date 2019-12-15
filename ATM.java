3e52dd93de3672ad243f5f73571ef2607c2d307b
surya-b21 <58516233+surya-b21@users.noreply.github.com>
2019-12-15 16:29:14 +0700

    Update ATM Cardless
    ini kodingan yang terbaru ya teman2. Untuk masalah error pada udah fix in. Kalo ada yang mau nambahin comment pada codingan nya gpp. Besok senin aku jelasin beberapa kekurangannya

--- ATM.java
+++ ATM.java
@@ -8,7 +8,7 @@ public class ATM {
 		// database
 		String[] user = { "Joni", "Jono" };
 		String[] pin = { "1234", "4321" };
-		int[] norek = { 00001, 00002 };
+		String[] norek = { "00001" , "00002" };
 		int[] saldo = { 5000000, 10000000 };
 		int[] code = new int[5];
 		boolean exitstatus = false;
@@ -29,7 +29,7 @@ public class ATM {
 					int menu = input.nextInt();
 					switch (menu) {
 					case 1:
-						System.out.print("Saldo anda adalah " + saldo[i]);
+						System.out.println("Saldo anda adalah " + saldo[i]);
 						break;
 					case 2:
 						System.out.println("Pilih jumlah paket tunai :");
@@ -60,32 +60,24 @@ public class ATM {
 						break;
 					case 3:
 						System.out.println("Silahkan isi nomor rekening tujuan anda!");
-						int rek = input.nextInt();
-						boolean checker = false;
-						int j = 0;
-						while (j < norek.length) {
-							if (rek == norek[j]) {
-								checker = true;
+						String rek = input.next();
+						for (int j=0; j<norek.length; j++) {
+							if (rek.equals(norek[j])){
+								transfer(saldo, i, j, user, norek);
 							}
-							j++;
 						}
-						if (checker = true) {
-							transfer(saldo, i, j, user, norek);
-						}
-						break;
 					}
-				} else {
-					System.out.println("ID/PIN anda salah");
-				}
-				System.out.println("Apakah anda ingin melakukan tansaksi lagi ?");
-				System.out.println("Ya/Tidak");
-				String lagi = input.next();
-				if ((lagi.equals("Ya"))||(lagi.equals("ya"))) {
-					exitstatus = false;
-				} else if ((lagi.equals("Tidak"))||(lagi.equals("tidak"))) {
-					exitstatus = true;
 				}
 			}
+			System.out.println("ID/PIN anda salah");
+			System.out.println("Apakah anda ingin melakukan tansaksi lagi ?");
+					System.out.println("Ya/Tidak");
+					String lagi = input.next();
+					if ((lagi.equals("Ya"))||(lagi.equals("ya"))) {
+						exitstatus = false;
+					} else if ((lagi.equals("Tidak"))||(lagi.equals("tidak"))) {
+						exitstatus = true;
+					}
 		}
 	}
 
@@ -112,24 +104,25 @@ public class ATM {
 		return saldoakhir;
 	}
 
-	public static int transfer(int saldo[], int i, int j, String user[], int norek[]) {
+	public static int transfer(int saldo[], int i, int j, String user[], String[] norek) {
+		int saldopengirim = saldo[i];
+		int saldopenerima = saldo[j];
 		boolean exit = false;
-		while (exit = false) {
+		while (exit == false) {
 			System.out.println("Silahkan masukkan nominal yang akan ditransfer");
 			int nominal = input.nextInt();
 			System.out.println("Apakah anda yakin angka mentransfer uang sebesar Rp. " + nominal + "\nKepada :"
 					+ user[j] + "\nDengan Nomor Rekening : " + norek[j]);
-			System.out.println("Ya/Tidak");
-			String tanya = input.next();
-			if ((tanya.equals("Ya")) || (tanya.equals("ya"))) {
-				return saldo[j] += nominal;
-			} else if ((tanya.equals("Tidak")) || (tanya.equals("tidak"))) {
-				exit = false;
+				System.out.println("Ya/Tidak");
+				String tanya = input.next();
+				if ((tanya.equals("Ya")) || (tanya.equals("ya"))) {
+					System.out.println("Transfer berhasil");
+					exit = true;
+					return (saldopenerima += nominal);
+				} else if ((tanya.equals("Tidak")) || (tanya.equals("tidak"))) {
+					exit = false;
+				}
 			}
-			System.out.println("Transfer berhasil");
-			exit = true;
-		}
-		return saldo[j];
-
+		return saldopenerima;
 	}
 }
